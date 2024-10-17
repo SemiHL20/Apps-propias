65db3e676566f99ce1d3c318c88481b6532b5d4b
SemiHL20 <semilopezsanchez@gmail.com>
2024-10-17 14:39:04 +0200

    nuevo

---
+++ TrackCloud/app/src/main/java/semi/android/trackcloud/MyWebViewClient.kt
@@ -0,0 +1,25 @@
+package semi.android.trackcloud
+
+import android.net.Uri
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+class MyWebViewClient : WebViewClient() {
+    override fun shouldOverrideUrlLoading(view: WebView,
+                      request: WebResourceRequest): Boolean {
+        // Verifica si la URL es segura
+        return if (isSafeUrl(request.url)) {
+            // Carga la URL en el WebView
+            false
+        } else {
+            // Do nothing
+            true
+        }
+    }
+
+    private fun isSafeUrl(url: Uri): Boolean {
+        // Lógica para verificar si la URL es segura
+        return url.scheme == "https" && url.host == "trackcloud.es"
+    }
+}
